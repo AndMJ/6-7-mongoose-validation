16b3ff22b84874e26161c6eb9b876a3e77586d4c
AndMJ <andrejoao070@gmail.com>
2024-12-02 16:44:59 +0000

    UPDATE messing around with validation

--- index.js
+++ index.js
@@ -10,10 +10,33 @@ connection.then(result => {
 
 //Class schema definition
 const coursesSchema = new mongoose.Schema({
-    name: { type: String, required: true},
+    name: { type: String, required: true, minLength: 5, maxLength: 72},
     author: String,
-    price: {type: Number, default: 10},
-    tags: [String],
+    price: {
+        type: Number,
+        default: 10,
+        required:
+        //we can set the price to be required if the course is published
+            function () {
+                /*we can do this. because the schema is a class*/
+                return this.isPublished //so this returns the current isPublished bool value
+            }
+    },
+    category: {
+        type: String,
+        enum: ["category1","category2","category3"]
+    },
+    tags: {
+        /*custom validation*/
+        type: Array,
+        validate: {
+            validator: function (value) {
+                return (value !== null && value.length > 0)
+            },
+            message: "A course should have at least 1 tag!"
+
+        }
+    },
     dateCreated: {type: Date, default: Date.now()},
     isPublished: Boolean
 })
@@ -35,10 +58,11 @@ const createCourse = async (course) => {
 }
 
 const course = {
-    //name: "mongoose course",
+    name: "mongoose course",
     author: "joaquim",
     price: 13,
-    tags: ["mongodb", "mongoose"],
+    tags: null,//["mongodb", "mongoose"]
+    category: "category1",
     isPublished: false
 }
 createCourse(course).then(result => {
